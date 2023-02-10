a409372718fb1843a794e75dc0be3e5899a87715
devarsh-sanghvi <devarsh@simformsolutions.com>
2023-02-10 17:50:15 +0530

    quries performed on 10 Feb

---
+++ MSSQL TUTORIAL/date wise queries/10 Feb 2023.sql
@@ -0,0 +1,385 @@
+﻿-- recursive query
+WITH cte_numbers(n, weekday) 
+AS (
+    SELECT 
+        0, 
+        DATENAME(DW, 0)
+    UNION ALL
+    SELECT    
+        n + 1, 
+        DATENAME(DW, n + 1)
+    FROM    
+        cte_numbers
+    WHERE n < 6
+)
+SELECT 
+    weekday
+FROM 
+    cte_numbers;
+
+-- PIVOT
+	SELECT * FROM   
+(
+    SELECT 
+        category_name, 
+        product_id,
+		model_year
+    FROM 
+        production.products p
+        INNER JOIN production.categories c 
+            ON c.category_id = p.category_id
+) t 
+PIVOT(
+    COUNT(product_id) 
+    FOR category_name IN (
+        "Children Bicycles", --column names
+        [Comfort Bicycles], 
+        [Cruisers Bicycles], 
+        [Cyclocross Bicycles], 
+        [Electric Bikes], 
+        [Mountain Bikes], 
+        [Road Bikes])
+) AS pivot_table;
+
+-- 
+DECLARE 
+    @columns NVARCHAR(MAX) = '';
+
+SELECT 
+    @columns += QUOTENAME(category_name) + ','
+FROM 
+    production.categories
+ORDER BY 
+    category_name;
+
+SET @columns = LEFT(@columns, LEN(@columns) - 1);
+PRINT @columns;
+go
+
+DECLARE 
+    @columns NVARCHAR(MAX) = '', 
+    @sql     NVARCHAR(MAX) = '';
+
+-- select the category names
+SELECT 
+    @columns+=QUOTENAME(category_name) + ','
+FROM 
+    production.categories
+ORDER BY 
+    category_name;
+
+-- remove the last comma
+SET @columns = LEFT(@columns, LEN(@columns) - 1);
+
+-- construct dynamic SQL
+SET @sql ='
+SELECT * FROM   
+(
+    SELECT 
+        category_name, 
+        model_year,
+        product_id 
+    FROM 
+        production.products p
+        INNER JOIN production.categories c 
+            ON c.category_id = p.category_id
+) t 
+PIVOT(
+    COUNT(product_id) 
+    FOR category_name IN ('+ @columns +')
+) AS pivot_table;';
+
+-- execute the dynamic SQL
+EXECUTE sp_executesql @sql;
+go
+
+SELECT 
+    *
+FROM 
+    master.sys.databases
+ORDER BY 
+    name;
+
+EXEC sp_databases;
+
+-- insert top 10
+create table sales.addresses(street text,city text,state text,zip_code numeric);
+INSERT TOP (10) 
+INTO sales.addresses (street, city, state, zip_code) 
+SELECT
+    street,
+    city,
+    state,
+    zip_code
+FROM
+    sales.customers
+ORDER BY
+    first_name,
+    last_name;
+
+--  Select Top 
+SELECT TOP 10
+    product_name, 
+    list_price
+FROM
+    production.products
+ORDER BY 
+    list_price DESC;
+
+SELECT TOP 1 percent
+    product_name, 
+    list_price
+FROM
+    production.products
+ORDER BY 
+    list_price DESC;
+
+SELECT TOP 3 WITH TIES
+    product_name, 
+    list_price
+FROM
+    production.products
+ORDER BY 
+    list_price DESC;
+go
+
+-- Delete top 10
+DELETE TOP (10) FROM production.products;
+DELETE TOP (1) PERCENT FROM production.products;
+go
+
+-- new table
+CREATE TABLE sales.category (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL,
+    amount DECIMAL(10 , 2 )
+);
+
+INSERT INTO sales.category(category_id, category_name, amount)
+VALUES(1,'Children Bicycles',15000),
+    (2,'Comfort Bicycles',25000),
+    (3,'Cruisers Bicycles',13000),
+    (4,'Cyclocross Bicycles',10000);
+
+
+CREATE TABLE sales.category_staging (
+    category_id INT PRIMARY KEY,
+    category_name VARCHAR(255) NOT NULL,
+    amount DECIMAL(10 , 2 )
+);
+
+
+INSERT INTO sales.category_staging(category_id, category_name, amount)
+VALUES(1,'Children Bicycles',15000),
+    (3,'Cruisers Bicycles',13000),
+    (4,'Cyclocross Bicycles',20000),
+    (5,'Electric Bikes',10000),
+    (6,'Mountain Bikes',10000);
+go
+
+-- Merge 
+MERGE sales.category t 
+    USING sales.category_staging s
+ON (s.category_id = t.category_id)
+WHEN MATCHED
+    THEN UPDATE SET 
+        t.category_name = s.category_name,
+        t.amount = s.amount
+WHEN NOT MATCHED BY TARGET 
+    THEN INSERT (category_id, category_name, amount)
+         VALUES (s.category_id, s.category_name, s.amount)
+WHEN NOT MATCHED BY SOURCE 
+    THEN DELETE;
+
+create table sales.test (test_id int);
+-- rename table
+EXEC sp_rename 'sales.test' , 'test2';
+
+--temp tables
+CREATE TABLE #haro_products (
+    product_name VARCHAR(MAX),
+    list_price DEC(10,2)
+);
+
+select * into #temp_test from sales.test;
+
+-- global temp table accross all connections
+CREATE TABLE ##heller_products (
+    product_name VARCHAR(MAX),
+    list_price DEC(10,2)
+);
+
+INSERT INTO ##heller_products
+SELECT
+    product_name,
+    list_price
+FROM 
+    production.products
+WHERE
+    brand_id = 3;
+
+drop table ##heller_products;
+
+select * from sales.test;
+CREATE SYNONYM orders FOR sales.orders;
+select * from orders;
+DROP SYNONYM IF EXISTS orders;
+
+-- datalength in bytes 
+SELECT
+    customer_id,
+    LEN(customer_id) len,
+    DATALENGTH(customer_id) data_length
+FROM
+    sales.orders;
+go
+
+-- store unicode char
+create schema test;
+go
+
+drop table if exists test.sql_server_nchar;
+CREATE TABLE test.sql_server_nchar (
+    val NCHAR(1) NOT NULL,
+	val2 NVARCHAR(10) NOT NULL
+);
+
+INSERT INTO test.sql_server_nchar (val,val2)
+output inserted.val , inserted.val2
+VALUES
+    (N'あ',N'こんにちは');
+select * from test.sql_server_nchar
+go
+
+-- Datetime2
+CREATE TABLE production.product_colors (
+    color_id INT PRIMARY KEY IDENTITY,
+    color_name VARCHAR (50) NOT NULL,
+    created_at DATETIME2
+);
+INSERT INTO production.product_colors (color_name, created_at)
+VALUES
+    ('Red', GETDATE());
+INSERT INTO production.product_colors (color_name, created_at)
+VALUES
+    ('Green', '2018-06-23 07:30:20');
+
+ALTER TABLE production.product_colors 
+ADD CONSTRAINT df_current_time 
+DEFAULT CURRENT_TIMESTAMP FOR created_at;
+
+INSERT INTO production.product_colors (color_name)
+VALUES
+    ('Blue');
+
+select * from production.product_colors;
+go
+
+-- disable a constraint rather than dropping
+ALTER TABLE test.products
+NOCHECK CONSTRAINT valid_price;
+
+-- complex case and group by 
+SELECT    
+    o.order_id, 
+    SUM(quantity * list_price) order_value,
+    CASE
+        WHEN SUM(quantity * list_price) <= 500 
+            THEN 'Very Low'
+        WHEN SUM(quantity * list_price) > 500 AND 
+            SUM(quantity * list_price) <= 1000 
+            THEN 'Low'
+        WHEN SUM(quantity * list_price) > 1000 AND 
+            SUM(quantity * list_price) <= 5000 
+            THEN 'Medium'
+        WHEN SUM(quantity * list_price) > 5000 AND 
+            SUM(quantity * list_price) <= 10000 
+            THEN 'High'
+        WHEN SUM(quantity * list_price) > 10000 
+            THEN 'Very High'
+    END order_priority
+FROM    
+    sales.orders o
+INNER JOIN sales.order_items i ON i.order_id = o.order_id
+WHERE 
+    YEAR(order_date) = 2018
+GROUP BY 
+    o.order_id;
+go
+
+--coalesce
+CREATE TABLE salaries (
+    staff_id INT PRIMARY KEY,
+    hourly_rate decimal,
+    weekly_rate decimal,
+    monthly_rate decimal,
+    CHECK(
+        hourly_rate IS NOT NULL OR 
+        weekly_rate IS NOT NULL OR 
+        monthly_rate IS NOT NULL)
+);
+
+
+SELECT
+    staff_id,
+    COALESCE(
+        hourly_rate*22*8, 
+        weekly_rate*4, 
+        monthly_rate
+    ) monthly_salary
+FROM
+    salaries;
+go
+
+DROP TABLE IF EXISTS sales.contacts;
+
+CREATE TABLE sales.contacts(
+    contact_id INT IDENTITY(1,1) PRIMARY KEY,
+    first_name NVARCHAR(100) NOT NULL,
+    last_name NVARCHAR(100) NOT NULL,
+    email NVARCHAR(255) NOT NULL,
+);
+INSERT INTO sales.contacts
+    (first_name,last_name,email) 
+VALUES
+    ('Syed','Abbas','syed.abbas@example.com'),
+    ('Catherine','Abel','catherine.abel@example.com'),
+    ('Kim','Abercrombie','kim.abercrombie@example.com'),
+    ('Kim','Abercrombie','kim.abercrombie@example.com'),
+    ('Kim','Abercrombie','kim.abercrombie@example.com'),
+    ('Hazem','Abolrous','hazem.abolrous@example.com'),
+    ('Hazem','Abolrous','hazem.abolrous@example.com'),
+    ('Humberto','Acevedo','humberto.acevedo@example.com'),
+    ('Humberto','Acevedo','humberto.acevedo@example.com'),
+    ('Pilar','Ackerman','pilar.ackerman@example.com');
+
+-- delete duplicates using cte (cannot do in postgres)
+WITH cte AS (
+    SELECT 
+        contact_id, 
+        first_name, 
+        last_name, 
+        email, 
+        ROW_NUMBER() OVER (
+            PARTITION BY 
+                first_name, 
+                last_name, 
+                email
+            ORDER BY 
+                first_name, 
+                last_name, 
+                email
+        ) row_num
+     FROM 
+        sales.contacts
+)
+DELETE FROM cte
+WHERE row_num > 1;
+go
+
+-- create and redefine view
+create or alter view num_10 as select 5 num_10;
+go
+
+-- rename view
+exec sp_rename 'num_10' , 'num_5';
